46d0c995fbba20257cf4f17587ee77f62d95118c
Salmela Leena <leena.salmela@cs.helsinki.fi>
2019-04-10 10:25:36 +0300

    moved license file and added preambles to eval scripts

--- eval/print-ovl.py
+++ eval/print-ovl.py
@@ -1,3 +1,22 @@
+#  Elmeri
+#
+#  Copyright (C) 2019 Leena Salmela
+#
+#  Contact: leena.salmela@cs.helsinki.fi
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Reads the positions of each Rmap and outputs pairs of Rmaps that
 # overlap by at least K fragments and EL kbp. Output is in dot format.
 # The Rmaps should be sorted according to their starting position.
